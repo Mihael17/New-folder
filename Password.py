aa60bd53f9e26c24e633096a6c799b58540f46d2
Mihael17 <mihaelgubina@gmail.com>
2023-09-12 12:45:10 +0200

    Second commit

--- Password.py
+++ Password.py
@@ -3,36 +3,35 @@ import string
 
 
 def main():
-    print("Password Generator\n")
+    print("Password Generator")
     
-    try:
-        length = int(
-            input("How long do you want your password to be (minimum of 8 number)")
-        )
-    except:
-        print("Input numners only")
-    password_length = 0
-    while password_length < length:
-        print("Your password must have at least 8 characters")
-        password_length = getPasswordLength()
+    #try:
+        #length = int(
+            #input("How long do you want your password to be (minimum of 8 number)"))
+                    
+    #except:
+        #print("Input numbers only")
+    #password_length = 0
+    #while password_length < length:
+        #print("Your password must have at least 8 characters")
+    password_length = get_Password_Length()
 
-    all_characters = getCharacters()
-    password = generatePassword(all_characters, password_length)
+    all_characters = get_Characters()
+    password = generate_Password(all_characters, password_length)
     print("\nYour password is: " + password)
     
 
 
-def getPasswordLength():
-    password_length = int(input("\nEnter the length of password: "))
-    if password_length < 8:
+def get_Password_Length():
+    password_length = int(input("\nHow long do you want your password to be (minimum of 8 number)"))
+    while password_length < 8:
         print("Your password must have at least 8 characters\n")
         password_length = int(input("Enter the length of password: "))
-        return password_length
-    else:        
-        return password_length
+    return password_length
+     
 
 
-def getCharacters():
+def get_Characters():
     lower = string.ascii_lowercase
     upper = string.ascii_uppercase
     num = string.digits
@@ -42,7 +41,7 @@ def getCharacters():
     return all_characters
 
 
-def generatePassword(all_characters, password_length):
+def generate_Password(all_characters, password_length):
     password = "".join(random.sample(all_characters, password_length))
     return password
 
